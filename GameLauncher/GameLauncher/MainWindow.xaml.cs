2d5aed0cec65ddbc3634e697354ddb0ec5244f89
Lucas Michon <wagashisakatsuki@gmailcom>
2023-06-20 09:53:47 +0200

    chore: initial commit

---
+++ GameLauncher/GameLauncher/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows;
+
+namespace GameLauncher
+{
+    enum LauncherStatus
+    {
+        ready,
+        failed,
+        downloadingGame,
+        downloadingUpdate
+    }
+
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string rootPath;
+        private string versionFile;
+        private string gameZip;
+        private string gameExe;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            rootPath = Directory.GetCurrentDirectory();
+            versionFile = Path.Combine(rootPath, "Version.txt");
+            gameZip = Path.Combine(rootPath, "Build.zip");
+            gameExe = Path.Combine(rootPath, "Game.exe");
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
